3700cd4b847c80f83011408bb6552b4b240680b0
Oleksandra Boyko <alexandrasboyko@gmail.com>
2024-12-07 16:42:51 +0200

    site continuation

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(function() {
+  const header = document.querySelector('.header');
+  window.onscroll = () => {
+    if(window.pageYOffset > 50) {
+      header.classList.add('header_active')
+    }
+    else {
+      header.classList.remove('header_active')
+    }
+  }
+}())
+
+  (function() {
+    const burgerItem = document.querySelector('.burger');
+    burgerItem.addEventListener('click', () => {
+
+    })
+  }())
